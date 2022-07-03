6f69777965cd4b17b1426cf407df9d716954ee23
ShrikantJawla <shrikantjawla@gmail.com>
2022-07-03 16:50:09 +0530

    Finding unique elements in an array without using extra space.

---
+++ findingUniqueForLoop.js
@@ -0,0 +1,30 @@
+let arr1=["Shrikant","Mohan", "Kunal","Shrikant","Mohan", "Manoj"];
+let numbers=[2,3,4,5,6,7,7];
+
+// arr1.splice(2,1,"now",2,"hello","Now",45);
+// console.log(arr1);
+
+
+//Finding unique using for loop:
+let num=[2,3,4,5,6,6,6,7,2,3,1]
+
+// for (let i= 0; i<num.length-1; i++) {
+//     for(let j=i+1; j<num.length; j++){
+//         if(num[i]===num[j]){
+//             num.splice(j,1);
+//             i--;
+//         }
+//     }
+// }
+// console.log(num);
+
+//Finding unique using for loop:
+for(let i=0; i<arr1.length; i++){
+    for(let j=i+1; j<arr1.length; j++){
+        if(arr1[i]===arr1[j]){
+            arr1.splice(j,1);
+            i--;
+        }
+    }
+}
+console.log(arr1);
